08d2eeb51a6a77ec57ea8ebbcbded79a29527d0a
romir077 <romir.mehta@gmail.com>
2022-09-26 23:28:20 +0530

    Added Models

--- src/main/java/com/InstiCab/models/Driver.java
+++ src/main/java/com/InstiCab/models/Driver.java
@@ -10,4 +10,67 @@ public class Driver {
     private String bankName;
     private User user;
 
+    public Long getDriverId() {
+        return driverId;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public String getAadharNumber() {
+        return aadharNumber;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public String getIfscCode() {
+        return ifscCode;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setDriverId(Long driverId) {
+        this.driverId = driverId;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public void setAadharNumber(String aadharNumber) {
+        this.aadharNumber = aadharNumber;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public void setIfscCode(String ifscCode) {
+        this.ifscCode = ifscCode;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/InstiCab/models/FavouriteLocation.java
+++ src/main/java/com/InstiCab/models/FavouriteLocation.java
@@ -3,6 +3,31 @@ package com.InstiCab.models;
 public class FavouriteLocation {
     private Long locationId;
     private float latitudeLocation;
+
+    public Long getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(Long locationId) {
+        this.locationId = locationId;
+    }
+
+    public float getLatitudeLocation() {
+        return latitudeLocation;
+    }
+
+    public void setLatitudeLocation(float latitudeLocation) {
+        this.latitudeLocation = latitudeLocation;
+    }
+
+    public float getLongitudeLocation() {
+        return longitudeLocation;
+    }
+
+    public void setLongitudeLocation(float longitudeLocation) {
+        this.longitudeLocation = longitudeLocation;
+    }
+
     private float longitudeLocation;
 
 }
