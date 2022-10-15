53ae17c4ba9d8fadce39b640f98f5effde39ca76
sherdil0023 <darshil.agrawal.cse20@itbhu.ac.in>
2022-10-15 14:32:22 +0530

    Add logout feature

---
+++ src/main/java/com/InstiCab/controllers/LogoutController.java
@@ -0,0 +1,15 @@
+package com.InstiCab.controllers;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogoutController {
+
+    @GetMapping("/logout")
+    public String logout(){
+        SecurityContextHolder.clearContext();
+        return "redirect:/";
+    }
+}
