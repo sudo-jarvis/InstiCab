b7654960fdcef0d7b0b0d893ce25c0b905761a88
Dhruv Singh <dhruv1778@gmail.com>
2022-10-29 14:37:54 +0530

    Fixed Stripe Payment Response

--- src/main/java/com/InstiCab/service/Implementation/StripeServiceImpl.java
+++ src/main/java/com/InstiCab/service/Implementation/StripeServiceImpl.java
@@ -5,11 +5,14 @@ import com.InstiCab.service.StripeService;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Charge;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -17,17 +20,19 @@ public class StripeServiceImpl implements StripeService {
 
     @Value("${STRIPE_SECRET_KEY}")
     String secretKey;
-
-    public StripeServiceImpl() {
-        Stripe.apiKey = secretKey;
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey= secretKey;
     }
-    public Charge charge(ChargeRequest chargeRequest)
+    public PaymentIntent charge(ChargeRequest chargeRequest)
             throws StripeException {
+        List<Object> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
         Map<String, Object> chargeParams = new HashMap<>();
         chargeParams.put("amount", chargeRequest.getAmount());
         chargeParams.put("currency", chargeRequest.getCurrency());
         chargeParams.put("description", chargeRequest.getDescription());
-        chargeParams.put("source", chargeRequest.getStripeToken());
-        return Charge.create(chargeParams);
+        chargeParams.put("payment_method_types", paymentMethodTypes);
+        return PaymentIntent.create(chargeParams);
     }
 }
