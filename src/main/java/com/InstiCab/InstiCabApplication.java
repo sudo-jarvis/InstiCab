5bf0d04cd0f1c47f031acd05f700ca22113cce3e
Dhruv Singh <dhruv1778@gmail.com>
2022-11-09 10:05:57 +0530

    Final Commit

--- src/main/java/com/InstiCab/InstiCabApplication.java
+++ src/main/java/com/InstiCab/InstiCabApplication.java
@@ -56,30 +56,4 @@ public class InstiCabApplication implements CommandLineRunner {
         resourceDatabasePopulator.execute(dataSource);
 	}
 
-
-//	@Bean
-//	public ServletWebServerFactory servletContainer() {
-//		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-//			@Override
-//			protected void postProcessContext(Context context) {
-//				SecurityConstraint securityConstraint = new SecurityConstraint();
-//				securityConstraint.setUserConstraint("CONFIDENTIAL");
-//				SecurityCollection collection = new SecurityCollection();
-//				collection.addPattern("/*");
-//				securityConstraint.addCollection(collection);
-//				context.addConstraint(securityConstraint);
-//			}
-//		};
-//		tomcat.addAdditionalTomcatConnectors(redirectConnector());
-//		return tomcat;
-//	}
-
-//	private Connector redirectConnector() {
-//		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-//		connector.setScheme("http");
-//		connector.setPort(80);
-//		connector.setSecure(false);
-//		connector.setRedirectPort(443);
-//		return connector;
-//	}
 }
